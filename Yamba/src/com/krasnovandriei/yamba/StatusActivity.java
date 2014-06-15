b83412cf1c57b963ea7b39fee1f221241a2170de
ae-krasnov <krasnovandriei@gmail.com>
2014-06-15 17:55:27 +0400

    The first commit

---
+++ Yamba/src/com/krasnovandriei/yamba/StatusActivity.java
@@ -0,0 +1,16 @@
+package com.krasnovandriei.yamba;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class StatusActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+	}
+
+
+}
